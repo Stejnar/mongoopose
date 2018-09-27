c15bb774dc4fd6aafcadd50165ffebc0ad70d380
Armin Butkereit <a.butkereit@gmail.com>
2018-09-27 02:36:43 +0200

    added types

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+declare class Params<T> {
+    as: object;
+    select: object;
+    query: object;
+    save: object;
+    result: T;
+
+    add(result: object, name: string): Params<any>
+    assign(obj: object): Params<any>
+    toError(error): Error
+}
+
+type Pipe = (params: Params<any>) => Params<any>;
+
+type Functor = (query?: (params: Params<any>) => Params<any>) =>
+    (pipe: Params<any>) =>
+        Promise<Params<any>>;
+
+type Action = (
+    resolve: (params: Params<any>) => void,
+    reject: (err: Error) => void,
+    params: Params<any>
+) => void;
+
+type Pipeline = (
+    action: (
+        resolve: (params: Params<any>) => void,
+        reject: (err: Error) => void,
+        params: Params<any>) => void
+) => (pipe: Params<any>) => Promise<Params<any>>;
+
+declare class Model {
+    findById: Functor;
+    findOne: Functor;
+    find: Functor;
+    update: Functor;
+    save: Functor;
+    pipe: Pipeline;
+}
+
+declare module '@stejnar/mongoopose' {
+    import mongoose from 'mongoose';
+
+    export function model(model: mongoose.Model<any>): Model;
+
+    export function compose(funcs: Functor[] | Pipeline[]): (params: Params<any>) => Promise<Params<any>>;
+
+    export function Params(obj?: object): Params<any>;
+}
